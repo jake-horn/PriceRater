50871e9521cb65a26ead3da43ad6374c9e4df3e3
Jake <spawny08@hotmail.com>
2023-07-07 10:03:59 +0100

    initial API project creation

---
+++ PriceRater.API/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PriceRater.API.Controllers
+{
+    [ApiController]
+    [Route("api/product")]
+    public class ProductController : ControllerBase
+    { 
+
+    }
+}
