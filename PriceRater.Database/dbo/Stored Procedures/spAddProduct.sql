1c9a59ecbd36c2acecfe1d8dd96caf38a55dcaf4
Jake <spawny08@hotmail.com>
2023-07-14 08:55:47 +0100

    Add database and procs to source control

---
+++ PriceRater.Database/dbo/Stored Procedures/spAddProduct.sql
@@ -0,0 +1,42 @@
+﻿-- =============================================
+-- Author:		Jake Horn
+-- Create date: 30/05/2023
+-- Description:	Adds product to dbo.Product table
+-- =============================================
+CREATE PROCEDURE [dbo].[spAddProduct] 
+	-- Add the parameters for the stored procedure here
+	@Title NVARCHAR(500), 
+	@Price DECIMAL(7,2), 
+	@WebAddress NVARCHAR(500), 
+	@DateAdded DATETIME2, 
+	@DateUpdated DATETIME2, 
+	@RetailerId INT, 
+	@WebScrapingId INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    INSERT INTO dbo.Product
+	(
+		Title, 
+		Price, 
+		WebAddress, 
+		DateAdded, 
+		DateUpdated, 
+		RetailerId, 
+		WebScrapingId
+	)
+	VALUES
+	(
+		@Title, 
+		@Price, 
+		@WebAddress, 
+		@DateAdded, 
+		@DateUpdated, 
+		@RetailerId, 
+		@WebScrapingId
+	)
+
+END
---
+++ PriceRater.Database/dbo/Tables/WebScrapingList.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[WebScrapingList] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [WebAddress] NVARCHAR (500) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
