cda21bb08324f091be6bc5b24d0832be238aafd5
Jake <spawny08@hotmail.com>
2023-07-19 13:56:14 +0100

    New stored procedures for updating process
    - dbo.spCheckIfProductExists to check if a product exists already
    - dbo.spUpdateExistingProduct updates an existing product instead of adding a duplicate

---
+++ PriceRater.Database/dbo/Stored Procedures/spCheckIfProductExists.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Jake Horn
+-- Create date: 19th July 2023
+-- Description:	Checks if a product exists in dbo.Product, returns as a bit value
+-- =============================================
+CREATE PROCEDURE dbo.spCheckIfProductExists
+	-- Add the parameters for the stored procedure here
+	@WebScrapingId INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT CASE WHEN EXISTS 
+		(SELECT 1 FROM dbo.Product 
+		WHERE WebScrapingId = @WebScrapingId)
+	THEN CAST (1 AS BIT) 
+	ELSE CAST (0 AS BIT) 
+	END
+END
