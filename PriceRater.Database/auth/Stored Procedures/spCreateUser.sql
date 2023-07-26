f5afdd653d8664bdc326225a4c0d3a9474231908
Jake <spawny08@hotmail.com>
2023-07-26 15:30:04 +0100

    database updates for authentication api

---
+++ PriceRater.Database/auth/Stored Procedures/spCreateUser.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [auth].[spCreateUser] 
+	@Name NVARCHAR(150), 
+	@Email NVARCHAR(200), 
+	@Password NVARCHAR(200),
+	@UserId INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO auth.Users (sName, sEmail, sPassword)
+    VALUES (@Name, @Email, @Password);
+
+    -- Capture the newly inserted ID in the @ID OUTPUT parameter
+    SET @UserId = SCOPE_IDENTITY();
+
+    -- Return the inserted user object using SELECT
+    SELECT lUserId AS 'UserId', sName AS 'Name', sEmail AS 'Email', sPassword AS 'Password' FROM auth.Users WHERE lUserId = @UserId;
+END
---
+++ PriceRater.Database/auth/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [auth].[Users] (
+    [lUserId]   INT            IDENTITY (1, 1) NOT NULL,
+    [sName]     NVARCHAR (150) NULL,
+    [sEmail]    NVARCHAR (200) NULL,
+    [sPassword] NVARCHAR (200) NULL,
+    PRIMARY KEY CLUSTERED ([lUserId] ASC),
+    UNIQUE NONCLUSTERED ([sEmail] ASC)
+);
+
