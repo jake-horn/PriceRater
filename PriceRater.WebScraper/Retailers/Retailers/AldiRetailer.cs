8a6fddcb7c547b21a96a3db00671cfa655e32ac4
Jake <spawny08@hotmail.com>
2023-09-18 09:06:09 +0100

    Added RetailerBase To improve OOP and SOLID principles, added RetailerBase as the base class for all retailers now and in the future.

--- PriceRater.WebScraper/Retailers/Retailers/AldiRetailer.cs
+++ PriceRater.WebScraper/Retailers/Retailers/AldiRetailer.cs
@@ -1,17 +1,10 @@
-﻿using PriceRater.WebScraper.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PriceRater.WebScraper.Retailers.Retailers
+﻿namespace PriceRater.WebScraper.Retailers.Retailers
 {
-    public class AldiRetailer : IRetailer
+    public class AldiRetailer : RetailerBase
     {
-        public string RetailerName => "Aldi";
+        public override string RetailerName => "Aldi";
 
-        public bool MatchedAddress(string webAddress)
+        public override bool MatchedAddress(string webAddress)
         {
             return webAddress.Contains("aldi.co.uk");
         }
--- PriceRater.WebScraper/Retailers/Retailers/AsdaRetailer.cs
+++ PriceRater.WebScraper/Retailers/Retailers/AsdaRetailer.cs
@@ -1,17 +1,10 @@
-﻿using PriceRater.WebScraper.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PriceRater.WebScraper.Retailers.Retailers
+﻿namespace PriceRater.WebScraper.Retailers.Retailers
 {
-    public class AsdaRetailer : IRetailer
+    public class AsdaRetailer : RetailerBase
     {
-        public string RetailerName => "Asda"; 
+        public override string RetailerName => "Asda"; 
 
-        public bool MatchedAddress(string webAddress)
+        public override bool MatchedAddress(string webAddress)
         {
             return webAddress.Contains("asda.com");
         }
