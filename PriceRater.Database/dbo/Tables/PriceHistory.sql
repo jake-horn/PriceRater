de58ed9a0eca9dd91e12e877e5bcd9e1ab1e2f30
Jake <spawny08@hotmail.com>
2023-09-19 14:40:47 +0100

    Added ClubcardPrice to dbo.Product and dbo.PriceHistory

--- PriceRater.Database/dbo/Tables/PriceHistory.sql
+++ PriceRater.Database/dbo/Tables/PriceHistory.sql
@@ -2,12 +2,15 @@
     [Id]            INT            IDENTITY (1, 1) NOT NULL,
     [Title]         NVARCHAR (250) NOT NULL,
     [Price]         DECIMAL (7, 2) NOT NULL,
+    [ClubcardPrice] DECIMAL (7, 2) NULL,
     [WebAddress]    NVARCHAR (500) NOT NULL,
     [DateAdded]     DATETIME2 (7)  NOT NULL,
     [RetailerId]    INT            NOT NULL,
     [WebScrapingId] INT            NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([RetailerId]) REFERENCES [dbo].[Retailer] ([Id]),
-    FOREIGN KEY ([WebScrapingId]) REFERENCES [dbo].[WebScrapingList] ([Id])
+    CONSTRAINT [PK__PriceHis__3214EC071D0FFDFD] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK__PriceHist__Retai__6FE99F9F] FOREIGN KEY ([RetailerId]) REFERENCES [dbo].[Retailer] ([Id]),
+    CONSTRAINT [FK__PriceHist__WebSc__70DDC3D8] FOREIGN KEY ([WebScrapingId]) REFERENCES [dbo].[WebScrapingList] ([Id])
 );
+
+
 
