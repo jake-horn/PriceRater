6cc31658af8a23aacd8519fdaa7f8d57b5777b2e
Jake <spawny08@hotmail.com>
2023-09-27 11:21:13 +0100

    Updated ProductDTO to map JSON properties

--- PriceRater.Common/Models/ProductDTO.cs
+++ PriceRater.Common/Models/ProductDTO.cs
@@ -1,13 +1,28 @@
-﻿namespace PriceRater.Common.Models
+﻿using System.Text.Json.Serialization;
+
+namespace PriceRater.Common.Models
 {
     public class ProductDTO
     {
+        [JsonPropertyName("title")]
         public string? Title { get; set; }
+
+        [JsonPropertyName("price")]
         public decimal? Price { get; set; }
+
+        [JsonPropertyName("clubcardPrice")]
         public decimal? ClubcardPrice { get; set; }
+
+        [JsonPropertyName("webAddress")]
         public string WebAddress { get; set; }
+
+        [JsonPropertyName("dataAdded")]
         public DateTime DateAdded { get; set; }
+
+        [JsonPropertyName("DateUpdated")]
         public DateTime DateUpdated { get; set; }
+
+        [JsonPropertyName("retailerId")]
         public int RetailerId { get; set; }
     }
 }
