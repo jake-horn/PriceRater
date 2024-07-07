7d36fd6f63cc9b36d5c7aff1ce29324aedf3206b
Jake <spawny08@hotmail.com>
2024-07-07 11:46:12 +0100

    Removed unused authentication in API

--- PriceRater.API/Authentication/DTO/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PriceRater.API.Authentication.DTO
-{
-    public class LoginDTO
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- PriceRater.API/Authentication/DTO/RegisterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PriceRater.API.Authentication.DTO
-{
-    public class RegisterDTO
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
